2bc1b16d397158a7c44918558964c1afe9a3d73c
djesecoronel <101542051+djesecoronel@users.noreply.github.com>
2022-08-24 07:23:57 -0500

    Add files via upload

---
+++ Ejemplo2/Ejemplo2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ejercicio2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escribe tu nombre:");
+            String nombre = Console.ReadLine();
+
+            Console.WriteLine("");
+            Console.WriteLine("Escribe una ciudad:");
+            String ciudad = Console.ReadLine();
+
+            Console.WriteLine("");
+            Console.WriteLine("Hola " + nombre + " bienvenido a la ciudad de " + ciudad);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Ejemplo3/Ejemplo3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ejercicio3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite el primer numero: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("Digite el segundo mumero: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 >= num2)
+            {
+                Console.WriteLine("El primer numero es mayor o igual que el segundo");
+            }
+            else
+            {
+                Console.WriteLine("El segundo numero es mayor que el primero");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

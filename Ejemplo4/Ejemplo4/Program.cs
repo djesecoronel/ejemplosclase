9fbd8267c420064e9bd3407484a559c845399307
djesecoronel <101542051+djesecoronel@users.noreply.github.com>
2022-08-24 07:29:08 -0500

    Add files via upload

---
+++ Ejemplo4/Ejemplo4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ejercicio4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
